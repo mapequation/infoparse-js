a4a18edd993d1609a39b4b371f1f5dd31f5cc145
Anton Eriksson <anton.eriksson@umu.se>
2019-04-24 18:07:19 +0200

    Add tree.js

---
+++ src/tree.js
@@ -0,0 +1,28 @@
+import matchCodelength from "./codelength";
+
+
+export default function (lines) {
+  const result = {
+    codelength: null,
+    nodes: [],
+  };
+
+  lines.forEach((line) => {
+    if (!result.codelength) {
+      result.codelength = matchCodelength(line);
+    }
+    const match = line.match(/(\d(?::\d)+) (\d(?:\.\d+)?) "(.+)" (\d+)(?: (\d+))?/);
+    if (match) {
+      const [_, path, flow, name, id, physId] = match;
+      result.nodes.push({
+        path: path.split(":").map(Number),
+        flow: +flow,
+        name,
+        id: physId ? +physId : +id,
+        stateId: physId ? +id : null,
+      });
+    }
+  });
+
+  return result;
+}
