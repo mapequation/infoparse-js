5cad9469ac62d09840116568d32e9547132f3231
Anton Eriksson <anton.eriksson@umu.se>
2019-04-24 20:12:51 +0200

    Add parsers/state.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 export { parseClu } from "./parsers/clu";
 export { parseFTree } from "./parsers/ftree";
 export { parseMap } from "./parsers/map";
+export { parseState } from "./parsers/state";
 export { parseTree } from "./parsers/tree";
 
 import parseClu from "./parsers/clu";
 import parseFTree from "./parsers/ftree";
 import parseMap from "./parsers/map";
+import parseState from "./parsers/state";
 import parseTree from "./parsers/tree";
 
 
@@ -13,6 +15,7 @@ export const parsers = {
   clu: parseClu,
   ftree: parseFTree,
   map: parseMap,
+  net: parseState,
   tree: parseTree,
 };
 
---
+++ src/parsers/state.js
@@ -0,0 +1,48 @@
+export default function (lines, parseLinks = true) {
+  const result = {
+    vertices: [],
+    states: [],
+    links: [],
+  };
+
+  let context = "";
+
+  lines.forEach((line) => {
+    if (line.startsWith("*")) {
+      const match = line.match(/^\*(vertices|states|links)/i);
+      context = match ? match[1].toLowerCase() : "";
+    } else if (context === "vertices") {
+      const match = line.match(/(\d+) "(.+)"/);
+      if (match) {
+        const [_, id, name] = match;
+        result.vertices.push({
+          id: +id,
+          name,
+        });
+      }
+    } else if (context === "states") {
+      const match = line.match(/(\d+) (\d+)(?: "(.+)")?/);
+      if (match) {
+        const [_, id, physId, name] = match;
+        result.states.push({
+          id: +id,
+          physId: +physId,
+          name,
+        });
+      }
+    } else if (context === "links" && parseLinks) {
+      const match = line.match(/(\d+) (\d+)(?: (\d(?:\.\d+)?))?/);
+      if (match) {
+        const [_, source, target, weight] = match;
+        result.links.push({
+          source: +source,
+          target: +target,
+          weight: weight ? +weight : null,
+        });
+      }
+    }
+  });
+
+  return result;
+}
+
