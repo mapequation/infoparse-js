eae1cde7f8d63d0902c4bcdf818fff67a74cb80e
Anton Eriksson <anton.eriksson@umu.se>
2019-04-25 14:36:11 +0200

    Use Map of parsers instead of Object

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import parseTree from "./parsers/tree";
 
 export { parseClu, parseFTree, parseMap, parseState, parseTree };
 
-export const parsers = {
+export const parsers = new Map(Object.entries({
   clu: parseClu,
   ftree: parseFTree,
   map: parseMap,
   net: parseState,
   tree: parseTree,
-};
+}));
 
-export const validExtensions = Object.keys(parsers);
+export const validExtensions = [...parsers.keys()];
 
 export const isValidExtension = ext => validExtensions.includes(ext);
 
-export const getParserForExtension = ext => parsers[ext];
+export const getParserForExtension = ext => parsers.get(ext);
