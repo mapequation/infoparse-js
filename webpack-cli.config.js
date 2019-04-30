de06b8284de5b57f37d3e65039801bbe1207de39
Anton Eriksson <anton.eriksson@umu.se>
2019-04-30 16:53:59 +0200

    Add babel-loader

--- webpack-cli.config.js
+++ webpack-cli.config.js
@@ -9,4 +9,18 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     library: "infoparse",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,18 @@ module.exports = {
     library: "infoparse",
     libraryTarget: "umd",
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
